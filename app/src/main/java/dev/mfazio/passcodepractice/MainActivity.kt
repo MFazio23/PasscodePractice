d15ce0d2347f559e3d44ef4477c355f393a51fc4
Michael Fazio <mfazio@skylinetechnologies.com>
2019-08-23 12:17:02 -0500

    Minor cleanup

--- app/src/main/java/dev/mfazio/passcodepractice/MainActivity.kt
+++ app/src/main/java/dev/mfazio/passcodepractice/MainActivity.kt
@@ -3,15 +3,14 @@ package dev.mfazio.passcodepractice
 import android.content.Intent
 import android.content.SharedPreferences
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceManager
 import kotlinx.android.synthetic.main.activity_main.*
 
